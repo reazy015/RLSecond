a411f56b148fffe7c75f5864c643807c1ad4c18e
reazy015 <redshuhart015@gmail.com>
2018-06-06 14:27:50 +0300

    map bug fix

--- js/site-preloader.js
+++ js/site-preloader.js
@@ -1,13 +1,13 @@
 'use strict';
 
-window.preloader = (function () {
+window.preloader = (function () {	
 	window.onload = function() {
 		setTimeout(function() {
 			document.querySelector('.preloader').classList.add('preloader--fadeout');
 			document.querySelector('.page').classList.remove('page-loading');
 
 			setTimeout(function() {
-				document.querySelector('.preloader').style.display = 'none';
+				document.querySelector('.preloader').style.display = 'none';				
 			}, 700);
 		}, 1500);
 	}
