7de4a5f8c2d85d7d75fd9c4047cb51d27f5dd2e1
reazy015 <redshuhart015@gmail.com>
2018-06-13 20:45:46 +0300

    new calculator

--- js/week-table-switch.js
+++ js/week-table-switch.js
@@ -1,6 +1,6 @@
 'use strict';
 
-window.weekTableSwithc = (function () {
+window.weekTableSwitch = (function () {
   Date.prototype.addDays = function(days) {
     var dat = new Date(this.valueOf())
     dat.setDate(dat.getDate() + days);
@@ -13,16 +13,21 @@ window.weekTableSwithc = (function () {
     return dat;
   }
 
+  Date.prototype.addHour = function(h) {
+    this.setHours(this.getHours() + h);
+    return this;
+  }
+
   var mainTable = document.querySelector('.schedule-table');
   var tableRowTimeHeader = mainTable.querySelectorAll('tr:nth-of-type(n + 2) td:first-of-type');
   var orderPopup = document.querySelector('.popup-wrapper');
   var orderPopupOrderDate = orderPopup.querySelector('.popup-module__date');
-  var orderPopupOrderTime = orderPopup.querySelector('.time-input');
   var initialAvailableCells = document.querySelectorAll('.schedule-table tr:nth-of-type(n + 8) td');
   var forwardBtn = document.querySelector('.schedule-panel-date__btn--right');
   var backwardBtn = document.querySelector('.schedule-panel-date__btn--left');
   var dateInfoBlock = document.querySelector('.schedule-panel-date__info');
   var datesTableHeaders = document.querySelectorAll('.order-date');
+  var orderSumInput = document.querySelector('.order-price__sum');
   var shipTableToggle = document.querySelectorAll('.ship-choice-input');
   var popupModal = document.querySelector('.popup-wrapper ');
   var timeInputsList = document.querySelectorAll('.time-input');
@@ -31,10 +36,6 @@ window.weekTableSwithc = (function () {
   var sundayBlock = document.querySelector('.schedule-panel-date__info .sunday');
   var mondayHeader = mainTable.querySelector('.mon');
   var datesTableHeadersCounter = datesTableHeaders.length;
-  var shipTimeSchedule = [
-    ['11:00', '12:30', '14:00', '15:30', '17:00', '18:30', '20:00', '21:30', '23:00', '00:00'],
-    ['10:30', '12:00', '13:30', '15:00', '16:30', '18:00', '19:30', '21:00', '22:30', '00:00']
-  ];
   var monthDictionary = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль','Август', 'Сентябрь','Октябрь', 'Ноябрь'];
   var monthDictionarPopup = ['Января', 'Февраля', 'Марта', 'Апреля', 'Мая', 'Июня', 'Июля','Августа', 'Сентября','Октября', 'Ноября'];
   var daysDictionary = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];  
@@ -120,13 +121,99 @@ window.weekTableSwithc = (function () {
     }
   }
 
+  function createTimeListItem(time, nextDay, date) {
+    var listElement = document.createElement('li');
+    var listElementText = document.createElement('span');
+
+    console.log(nextDay);
+
+    if (time < 10) {
+      listElementText.textContent = '0' + time + ':00';     
+    } else {
+      listElementText.textContent = time + ':00';
+    }
+
+    if (nextDay) {
+      var day = date.getDate();
+      var month = date.getMonth() < 10 ? '0' + date.getMonth() : date.getMoth();
+
+      listElementText.textContent += ' ' + day + '.' + month;
+    }  
+
+
+    listElement.classList.add('variant-list-item');
+    listElement.setAttribute('data-time', listElementText.textContent);
+    listElement.setAttribute('data-date', date);
+    listElement.append(listElementText);
+
+    return listElement;
+  }
+
+  function setEndTimeList(beginTime, date) {
+    var initialDate = new Date(date);
+    var beginTimeHour = beginTime.replace(/[0:]/g, '');
+    date.setHours(beginTimeHour);
+   
+
+    for (var i = 0; i < 24; i++) {
+      date.addHour(1);
+
+      if (initialDate.getDate() < date.getDate()) {
+        timeSwitchList[1].appendChild(createTimeListItem(date.getHours(), true, date));
+      } else {
+        timeSwitchList[1].appendChild(createTimeListItem(date.getHours(), false, date));
+      }      
+    }    
+  }
+
+  function setBeginTimeList(date) {
+    var initialDate = new Date(date);
+    initialDate.setMinutes(0);
+   
+    for (var i = 0; i < 24; i++) {
+      initialDate.setHours(i);
+      timeSwitchList[0].appendChild(createTimeListItem(i, undefined, initialDate));
+    }
+  }
+
+  function formatEndTime(date) {
+    var hours = (new Date(date)).getHours();
+
+    if (hours < 10) {
+      return '0' + hours + ':00';
+    } else {
+      return hours + ':00';
+    }
+  }
+
+  function removeEndTimeList(elem) {
+    while(elem.firstElementChild) {
+      elem.removeChild(elem.firstElementChild);
+    }
+  }
+
   function onOrderHandleClick(item, date, time) {
     if (item) {
-      if (!item.dataset.disable) {
+      if (!item.dataset.disable) {        
+        var beginTimeDatasetValue = new Date(date.setHours(time.replace(/[0:]/g, '')));
+        var endTimeDatasetValue = new Date(date.setHours(time.replace(/[0:]/g, '')));
+        endTimeDatasetValue.addHour(1);
+        beginTimeDatasetValue.setMinutes(0);
+        endTimeDatasetValue.setMinutes(0);                 
+
         orderPopup.classList.add('popup-wrapper--open');
         document.body.classList.add('no-scroll');
         orderPopupOrderDate.textContent = daysDictionary[date.getDay()] + ', ' + date.getDate() + ' ' + monthDictionarPopup[date.getMonth()];
-        orderPopupOrderTime.value = time;
+        timeInputsList[0].setAttribute('data-date', beginTimeDatasetValue);
+        timeInputsList[1].setAttribute('data-date', endTimeDatasetValue);
+        timeInputsList[0].value = time;
+        timeInputsList[1].value = formatEndTime(endTimeDatasetValue);
+        orderSumInput.setAttribute('data-price', document.querySelector('.ship-choice-input:checked').dataset.price);
+        orderSumInput.value = document.querySelector('.ship-choice-input:checked').dataset.price;
+        
+        setBeginTimeList(date);
+        setEndTimeList(timeInputsList[1].value, endTimeDatasetValue);
+
       }
     }  else {
       document.body.classList.add('no-scroll');
@@ -147,8 +234,18 @@ window.weekTableSwithc = (function () {
     }
   }
 
+  function calculateFinalOrderSum(endTime, beginTime) {
+    endTime = new Date(endTime);
+    beginTime = new Date(beginTime);
+    var result = new Date(endTime.getTime() - beginTime.getTime());
+    result = result.getUTCHours();
+   
+    orderSumInput.value = Number(result) * Number(orderSumInput.dataset.price);
+    console.log(result);
+    return result;
+  }
+
   updateTableDateHeader();
-  markDisabledCells(initialAvailableCells);
 
 
   forwardBtn.addEventListener('click', function() {
@@ -168,32 +265,18 @@ window.weekTableSwithc = (function () {
     }
   });
 
-  shipTableToggle[1].addEventListener('change', function() {
-      mainTable.classList.add('schedule-table--ss');
-      mainTable.classList.remove('schedule-table--fs');
-      setTimeHeaders(1);
-      clearDisabledCell(initialAvailableCells);
-      markDisabledCells(mainTable.querySelectorAll('tr:nth-of-type(n + 9) td:nth-of-type(n + 2)'));
-  });
-
-  shipTableToggle[0].addEventListener('change', function() {
-    mainTable.classList.remove('schedule-table--ss');
-    mainTable.classList.add('schedule-table--fs');
-    setTimeHeaders(0);   
-    clearDisabledCell(mainTable.querySelectorAll('tr:nth-of-type(n + 9) td:nth-of-type(n + 2)'));
-    markDisabledCells(initialAvailableCells);
-  });
-
   mainTable.addEventListener('click', function(evt) {
     evt.stopPropagation();
     var currentDate = new Date();
     var target = evt.target;
     var index = Array.prototype.indexOf.call(target.parentNode.children, target)
-    var correspondingTheader = document.querySelector('.schedule-table th:nth-child(' + (index+1) + ')');
+    var correspondingTheader = document.querySelector('.schedule-table th:nth-child(' + (index + 1) + ')');
     var correspondingTime = target.parentElement.firstElementChild.dataset.time;
     var correspondingTimeHours = Number(correspondingTime.split(':')[0]);
     var correspondingTimeMinutes = Number(correspondingTime.split(':')[1]);
     var columnDate = new Date(correspondingTheader.dataset.date);
+
+   
     
     if (!Boolean(target.getAttribute('disabled'))) {   
 
@@ -220,13 +303,32 @@ window.weekTableSwithc = (function () {
 
   timeSwitchList[0].addEventListener('click', function(evt) {
     var target = evt.target;
-    timeInputsList[0].value = target.dataset.time;    
-  });  
+    var event = new Event('change');
+    timeInputsList[0].value = target.dataset.time;
+    timeInputsList[0].setAttribute('data-date', target.dataset.date);
 
-  timeSwitchList[1].addEventListener('click', function(evt) {
-    console.log(this);
+
+    this.dispatchEvent(event);
+  }); 
+
+  timeSwitchList[0].addEventListener('change', function() {
+    var initialDate = new Date(timeInputsList[0].dataset.date);
+    initialDate.addHour(1);
+
+    timeInputsList[1].value = formatEndTime(initialDate);
+    timeInputsList[1].dataset.date = initialDate;
+    removeEndTimeList(timeSwitchList[1]);
+    setEndTimeList(timeInputsList[1].value, initialDate);
+
+    calculateFinalOrderSum(timeInputsList[1].dataset.date, timeInputsList[0].dataset.date); 
+  });
+
+  timeSwitchList[1].addEventListener('click', function(evt) {    
     var target = evt.target;
-    timeInputsList[1].value = target.dataset.time;    
+    timeInputsList[1].value = target.dataset.time; 
+    timeInputsList[1].setAttribute('data-date', target.dataset.date);
+
+    calculateFinalOrderSum(timeInputsList[1].dataset.date, timeInputsList[0].dataset.date);
   });  
 
 })();
